0df865b44e7e12f42ba1d2e164a972310b02b089
oscar andres jurado ortiz <oscar.jurado@bancounion.com>
2023-10-19 23:17:37 -0500

    start proyect

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+//import React from 'react';
+import DynamicButton from '../src/components/button.jsx'; // Ajusta la ruta según tu estructura de directorios
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>welcome</h1>
+      <DynamicButton buttonText="Enviar" type="submit"/>
+      <DynamicButton buttonText="Cancelar" type="submit"/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/button.jsx
@@ -0,0 +1,11 @@
+
+//import React from 'react';
+// Define un componente de botón reutilizable
+// eslint-disable-next-line react/prop-types
+function DynamicButton({ buttonText }) {
+  return (
+    <button>{buttonText}</button>
+  );
+}
+
+export default DynamicButton;
